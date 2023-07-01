a5f9b134fc0415ee40c4b56d5e3d90569a25a5a0
Yangguang Li <liyangguangcn@gmail.com>
2023-06-30 21:40:39 -0700

    Clean up layouts.

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = `Yangguang's blog`;
-export const SITE_DESCRIPTION = 'Yangguang Li - Web dev';
+export const SITE_TITLE = `yangguang.li`;
+export const SITE_DESCRIPTION = 'Yangguang Li - Web dev, UX engineer';
