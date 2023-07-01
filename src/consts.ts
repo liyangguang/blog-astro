083807445e937423a1a327b1a13370da7f7b5fc8
Yangguang Li <liyangguangcn@gmail.com>
2023-07-01 12:18:08 -0700

    Misc

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = `yangguang.li`;
-export const SITE_DESCRIPTION = 'Yangguang Li - Web dev, UX engineer';
+export const SITE_TITLE = `Yangguang Li - Web dev, UX engineer`;
+export const SITE_DESCRIPTION = 'Yangguang Li - Web dev, UX engineer in SF Bay Area. I write about web developement and more.';
